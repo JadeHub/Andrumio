3a8e0c9be693daf90fc8f922bf7e43d6c3b1938f
JadeHub <jade.develop@gmail.com>
2015-01-10 20:00:33 +1100

    delete

--- app/src/main/java/com/josh/mpd/IArtist.java
+++
@@ -1,9 +0,0 @@
-package com.josh.mpd;
-
-import java.util.List;
-
-public interface IArtist {
-
-    String GetName();
-
-}
--- app/src/main/java/com/josh/mpd/ITrack.java
+++
@@ -1,9 +0,0 @@
-package com.josh.mpd;
-
-/**
- * Created by Josh on 4/01/2015.
- */
-public interface ITrack {
-
-    String GetName();
-}
